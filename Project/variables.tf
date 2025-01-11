7b8409d83aa92fbe8aa3bd7d820c9f353978028d
mikka-1123 <mikkasingh1123@gmail.com>
2025-01-11 09:53:12 +0000

    Added TF files to create VPC and Subnet  in AWS

---
+++ Project/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_config" {}
+variable "subnet_config" {}
+
+
+
+variable "region" {
+   
+}
+
+
+variable "access_key" {
+  
+}
+
+variable "secret_key" {
+  
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region where resources will be provisioned"
+  default     = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-053b12d3152c0cc71"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.large"
+}
